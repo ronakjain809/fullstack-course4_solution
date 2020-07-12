1b07f9e956a2442d40c33de52c90b6ae11f29367
ronakjain809 <ronak.jain809@gmail.com>
2020-07-12 22:09:57 +0530

    final update

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -1,36 +1,3 @@
-/*
-
-Solution of assignment 4:
-
-
-
-Expected output:
-
-
-
-Hello Yaakov
-
-Good Bye John
-
-Good Bye Jen
-
-Good Bye Jason
-
-Hello Paul
-
-Hello Frank
-
-Hello Larry
-
-Hello Paula
-
-Hello Laura
-
-Good Bye Jim
-
-*/
-
-
 
 (function() {
 
